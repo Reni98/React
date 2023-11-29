c04723fbc4c929861a940329aa2403795fe04cba
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-11-29 12:23:04 +0100

    Add files via upload

---
+++ Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './style.css'
+function Post({blog,key}) {
+  return (
+    <Link to={`/read/${blog.id}`} className='post-link'>
+    <div className='blog'>
+        <img src={blog.thumbnail} />
+        <div className='blog-title'>
+        <h2>{blog.title}</h2>
+        <p>{blog.description}</p>
+        </div>
+    </div>
+    </Link>
+  )
+}
+
+export default Post
